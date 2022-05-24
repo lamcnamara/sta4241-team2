f049ab48c9464f3498e43356e81ce029e93c3401
lamcnamara <85337692+lamcnamara@users.noreply.github.com>
2022-05-24 18:42:00 -0400

    Add files via upload

--- sta_assignment2.py
+++ sta_assignment2.py
@@ -3,36 +3,34 @@ Author: Team 2, STA4241 Summer 2022
 Name: sta_assignment2.py (STA 4241 - Assignment #2B)
 Date: 27 May 2022
 Description: Model performance functions, problems 3.1-3.6 and 4.1-4.5.
-22 May update: contains results for problem 1.1 and code for 3.1-3.5.
+24 May update: contains code for 3.1-3.5.
+Maintained by Lauren McNamara (mcnamaralaurenh@gmail.com).
 """
 
 import pandas as pd
 from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
 
 # Problem 3.1
-# Import CSV (when it's uploaded)
-# filename = "C:/Users/zjant/Documents/as2a-problem1-1-model1.csv"
-# data1 = pd.read_csv(filename)
+filename = "C:/Users/zjant/Downloads/House_Prices_PRED.csv"
+problem3data = pd.read_csv(filename)
 
 # Problem #3.2
-# Find residual sum of squares
-# Takes dataframe, observations field and predictions field
 def sse(df, obs, pred):
+    """Returns residual sum of squares (sum of squared error)."""
     ssr = 0
     for index, row in df.iterrows():
         ssr += (row[pred] - row[obs])**2
     return ssr
 
-# Find average squared error
-# Takes dataframe, observations field and predictions field
 def ase(df, obs, pred):
-    ave = sse(df, obs, pred)/df.shape[0] # .shape[0] is row count in pandas
+    """Returns average squared error."""
+    ave = sse(df, obs, pred)/df.shape[0] # .shape[0] is row count
     return ave
 
 # Problem 3.3
-# Find coefficient of determination (R^2)
-# Takes dataframe, observations field and predictions field
 def rsquared(df, obs, pred):
+    """Returns coefficient of determination R^2 (R-squared)."""
     obsmean = df[obs].mean()
     num = denom = 0
     for index, row in df.iterrows():
@@ -42,9 +40,9 @@ def rsquared(df, obs, pred):
     return r2
 
 # Problem 3.4
-# Find mean absolute percentage error
-# Takes dataframe, observations field and predictions field
 def mape(df, obs, pred):
+    """Returns mean absolute percentage error between 0 and 1.
+    Remember to multiply by 100 for percent!"""
     total = 0
     for index, row in df.iterrows():
         total += abs((row[obs] - row[pred])/row[obs])
@@ -52,40 +50,32 @@ def mape(df, obs, pred):
     return pcerr
 
 # Problem 3.5
-# Find mean absolute error
-# Takes dataframe, observations field and predictions field
 def mae(df, obs, pred):
+    """Returns mean absolute error."""
     total = 0
     for index, row in df.iterrows():
         total += abs(row[pred] - row[obs])
     abserr = total/df.shape[0]
     return abserr
 
-# Problem 1.1 data
-problem11data = {'response': [3, 4, 5, 6, 7], 
-                 'm1pred': [3.2, 4.3, 4.9, 5.7, 6.9], 
-                 'm2pred': [3.3, 4.2, 4.8, 5.9, 7.1]}
-problem11 = pd.DataFrame(problem11data)
+# 3.2 answer
+print("SSE:", round(sse(problem3data, "SalePrice", "P_SalePrice"), 4))
+print("ASE:", round(ase(problem3data, "SalePrice", "P_SalePrice"), 4))
+# 3.3 answer
+print("R^2:", round(rsquared(problem3data, "SalePrice", "P_SalePrice"), 4))
+# 3.4 answer
+print("MAPE:", round(mape(problem3data, "SalePrice", "P_SalePrice"), 4) * 100, "%")
+# 3.5 answer
+print("MAE:", round(mae(problem3data, "SalePrice", "P_SalePrice"), 4))
 
-# Problem 1.1-1
-print("SSE of model 1:", round(sse(problem11, "response", "m1pred"), 4))
-print("SSE of model 2:", round(sse(problem11, "response", "m2pred"), 4))
+"""Should output as follows:
+SSE: 740014639177.1655
+ASE: 506859341.9022
+R^2: 0.9196
+MAPE: 7.03 %
+MAE: 12470.8337
+"""
 
-# Problem 1.1-2
-print("ASE of model 1:", round(ase(problem11, "response", "m1pred"), 4))
-print("ASE of model 2:", round(ase(problem11, "response", "m2pred"), 4))
-
-# Problem 1.1-3
-print("R2 of model 1:", rsquared(problem11, "response", "m1pred"))
-print("R2 of model 2:", rsquared(problem11, "response", "m2pred"))
-
-# Problem 1.1-4
-print("MAPE of model 1:", round(mape(problem11, "response", "m1pred"), 4)*100, "%")
-print("MAPE of model 2:", round(mape(problem11, "response", "m2pred"), 4)*100, "%")
-
-# Problem 1.1-5
-print("MAE of model 1:", round(mae(problem11, "response", "m1pred"), 4))
-print("MAE of model 2:", round(mae(problem11, "response", "m2pred"), 4))
 
 """
 # TODOs
