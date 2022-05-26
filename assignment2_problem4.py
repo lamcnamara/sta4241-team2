240d61a44b1457298d1b2e15d3dcdd0a6d7d2c7e
lamcnamara <85337692+lamcnamara@users.noreply.github.com>
2022-05-25 21:46:34 -0400

    Update assignment2_problem4.py

--- assignment2_problem4.py
+++ assignment2_problem4.py
@@ -3,7 +3,7 @@ Author: Team 2, STA4241 Summer 2022
 Name: assignment2_problem4.py (STA 4241 - Assignment #2B)
 Date: 27 May 2022
 Description: Problems 4.1-4.5 code, answers, and plot.
-May 25 update: Problems 4.1-4.2.
+May 25 update: Problems 4.1-4.2, 4.4-4.5.
 """
 
 import pandas as pd
@@ -11,6 +11,8 @@ from pandas import Series, DataFrame
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.rcParams["figure.dpi"] = 150
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Problem 4.1
 filename = "C:/Users/zjant/Downloads/Microsoft_Results.csv"
@@ -39,12 +41,35 @@ def cmatrix_cutoff(df, actual, predictp, cutoff=0.5, places=3):
         mat.precision = round(mat.TP/(mat.TP+mat.FP), places)
     return mat
 
-# 5th percentiles
+# Example for cutoff of 0.7
+ex_p42 = cmatrix_cutoff(p4data, "HasDetections", "P_HasDetections", cutoff=0.7)
+print("--- Confusion Matrix ---")
+print("Cutoff probability:\t", ex_p42.cutoff)
+print("True positives:\t\t", ex_p42.TP)
+print("False positives:\t", ex_p42.FP)
+print("True negatives:\t\t", ex_p42.TN)
+print("False negatives:\t", ex_p42.FN)
+print("Sensitivity:\t\t", ex_p42.sensitivity)
+print("Specificity:\t\t", ex_p42.specificity)
+print("Accuracy:\t\t\t", ex_p42.accuracy)
+print("Precision:\t\t\t", ex_p42.precision)
+
+# Problem 4.3: AUC, Gini
+
+# Problem 4.4
 rcurve = {'x':[],'y':[]}
-rocurve = pd.DataFrame(rcurve)
-for d in range(1,21):
-    cut=d/20.
-    tm = cmatrix_cutoff(p4data, "HasDetections", "P_HasDetections", cutoff=cut, places=3)
-    rocurve=rocurve.append({'x':tm.tpr,'y':tm.fpr}, ignore_index=True)
-plt.plot(rocurve['x'],rocurve['y'])
+r10curve = pd.DataFrame(rcurve)
+for i in range(1,11):
+    part=i/10.
+    tm = cmatrix_cutoff(p4data, "HasDetections", "P_HasDetections", cutoff=part)
+    r10curve=r10curve.append({'x':tm.tpr,'y':tm.fpr}, ignore_index=True)
+
+# Problem 4.5
+r5curve = pd.DataFrame(rcurve)
+for i in range(1,21):
+    part=i/20.
+    tm = cmatrix_cutoff(p4data, "HasDetections", "P_HasDetections", cutoff=part)
+    r5curve=r5curve.append({'x':tm.tpr,'y':tm.fpr}, ignore_index=True)
+
+plt.plot(r5curve['x'], r5curve['y'])
 plt.show()
