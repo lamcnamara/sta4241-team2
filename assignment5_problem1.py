b8284de099ebcfd87ea173f4d01a04dc301664a3
lamcnamara <85337692+lamcnamara@users.noreply.github.com>
2022-06-16 19:37:09 -0400

    Update assignment5_problem1.py

--- assignment5_problem1.py
+++ assignment5_problem1.py
@@ -1,8 +1,8 @@
 """
 Author: Team 2, STA4241 Summer 2022
 Name: assignment5_problem1.py (STA 4241 - Assignment #5)
-Date: 16 June 2022
-Description: Problems 1 code, answers, and plot.
+Date: 17 June 2022
+Description: Problem 1 code, answers, and plot.
 """
 
 import pandas as pd
@@ -11,28 +11,31 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.rcParams["figure.dpi"] = 150
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+plt.rcParams["figure.autolayout"] = True
 import sklearn.metrics
 import sklearn.utils
 from sklearn import tree
 from sklearn.tree import DecisionTreeRegressor
 
-# Enable for reproducible results. Set to 1000
-np.random.seed(1000)
+np.random.seed(1000) # Enable for reproducible results. Set to 1000
 
-# Problem 1
 filename = "C:/Users/zjant/Downloads/ASS_05_Data.csv"
 housedata = pd.read_csv(filename)
+Xall = pd.DataFrame({'LotArea': housedata['LotArea'], 'TotalBsmtSF': housedata['TotalBsmtSF'], 'GarageCars': housedata['GarageCars'], 'AGE': housedata['AGE'], 'TotalArea': housedata['TotalArea']})
+Yall = pd.DataFrame({'SalePrice': housedata['SalePrice']})
 
+# Step 01
 # Sample with replacement n = 1460, 20 times, generating 20 samples
 samples = {}
 for k in range(1,21):
     samples[k] = sklearn.utils.resample(housedata, replace=True, n_samples=1460)
 
-# Generate the 20 models from each of the 20 samples
+# Generate the 20 models from each of the 20 samples, with predictions and model scores
 models = {}
-Xs = Ys = {}
+Xs = {}
+Ys = {}
+predicted = {}
+scores = {}
 for k in range(1,21):
     # Predictors
     Xs[k] = pd.DataFrame({'LotArea': samples[k]['LotArea'],
@@ -42,15 +45,37 @@ for k in range(1,21):
                           'TotalArea': samples[k]['TotalArea']})
     # Continuous response variable
     Ys[k] = pd.DataFrame({'SalePrice': samples[k]['SalePrice']})
-    
-    # sklearn decision tree regression, parameters are complete guesses - crossvalidate to find optimal params
-    models[k] = DecisionTreeRegressor(criterion="squared_error", max_depth=7, min_samples_split=25)
+    # Step 02
+    models[k] = DecisionTreeRegressor(criterion="squared_error", max_depth=6, min_samples_split=50) # Experiment with?
     models[k].fit(Xs[k], Ys[k])
+    # Step 03
+    predicted[k] = models[k].predict(Xall)
+    scores[k] = models[k].score(Xall, Yall)
 
-    #for index, row in samples[k].iterrows():
+# Step 04
+# Ensemble each of 20 model predictions, for each of 1460 observations, to get 1 bagging estimator for each of 1460 obs
+baggingest = []
+for i, row in housedata.iterrows():
+    total = 0
+    for k in predicted:
+        total += predicted[k][i]
+    baggingest.append(total/20)
 
-        #this_xs = pd.DataFrame([row['LotArea'], row['TotalBsmtSF'], row['GarageCars'], row['AGE'], row['TotalArea']])
-        #this_array = np.array([row['LotArea'], row['TotalBsmtSF'], row['GarageCars'], row['AGE'], row['TotalArea']])
-        #row['Pred_SalePrice'] = models[k].predict(this_xs)
-        #row['Tree_Score'] = models[k].score(this_array, row['SalePrice'])
-        # Trying to find out how to use predict() here
+# Step 05
+bagerror = 0
+for k in predicted:
+    for i, row in housedata.iterrows():
+        bagerror += ((predicted[k][i] - baggingest[i])**2)
+bagmse = bagerror/20
+print("Bagging error estimator:", bagmse)
+
+# Box plot for all 20 predictions + bagging ensemble
+predicted['ens'] = np.array(baggingest)
+#predicted['obs'] = np.array(housedata['SalePrice'])
+preds = pd.DataFrame(predicted)
+plt.style.use('seaborn-darkgrid')
+ax = preds.plot(kind="box")
+plt.title("20 trees and bagging ensemble")
+plt.xlabel("Estimator")
+plt.ylabel("HousePrice (predicted)")
+plt.show()
